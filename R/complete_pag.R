ba4a3d0e2e9904176ca7683bdaee9c752212262a
Apara Venkat <apara.vnkat@gmail.com>
2026-03-11 19:33:22 -0700

    Add complete_pag

--- R/complete_pag.R
+++ R/complete_pag.R
@@ -8,12 +8,12 @@
 #'  The matrix follows the convention in pcalg: 0 refers to no edge. 1 refers to circle edge mark.
 #'  2 refers to an arrowhead and 3 refers to a tail. For example, `pag[a, b] = 2` and `pag[b, a] = 1`
 #'  says that the edge is `a o-> b`.
-#' @param rules A boolean vector of length 13 indicating which rules are to be invoked
+#' @param rules A boolean vector of length 18 indicating which rules are to be invoked
 #' @param verbose Boolean indicating verboseness
 #' @param debug Boolean indicating verboseness level higher than verbose
 #' @returns A list with two named elements, `pag` and `counter`.
 #'  `pag` is the adjacency matrix obtained after completing all the rules turned on in the `rules` parameter.
-#'  `counter` is a vector of length 13 indicating how many times each rule was invoked.
+#'  `counter` is a vector of length 18 indicating how many times each rule was invoked.
 #' @seealso [expertOrientR::generate_mag_pag()] for format of adjacency matrices
 #' @export
 complete_pag <- function(pag, rules=NULL, verbose=FALSE, debug=FALSE) {
@@ -21,14 +21,15 @@ complete_pag <- function(pag, rules=NULL, verbose=FALSE, debug=FALSE) {
         storage.mode(pag) <- "numeric"
     }
     p <- as.numeric(dim(pag)[1])
-    num.rules <- 13
-    counter <- rep.int(0, 13)
+    num.rules <- 18
+    counter <- rep.int(0, num.rules)
     old_pag <- matrix(0, p, p)
     
     if (is.null(rules)) {
         rules <- c(TRUE, TRUE, FALSE, TRUE, FALSE,
                    FALSE, FALSE, TRUE, FALSE, TRUE,
-                   FALSE, TRUE, TRUE)
+                   FALSE, TRUE, TRUE, TRUE, FALSE,
+                   FALSE, FALSE, TRUE)
     }
     
     
@@ -45,9 +46,22 @@ complete_pag <- function(pag, rules=NULL, verbose=FALSE, debug=FALSE) {
         c("Rule 10", expertOrientR:::rule.10),
         c("Rule 11", expertOrientR:::rule.11),
         c("Rule 12", expertOrientR:::rule.12),
-        c("Rule 13", expertOrientR:::rule.13)
+        c("Rule 13", expertOrientR:::rule.13),
+        c("Rule 14", expertOrientR:::rule.14),
+        c("Rule 15", rule.null),      # Not implemented
+        c("Rule 16", rule.null),      # Not implemented
+        c("Rule 17", rule.null),      # Not implemented
+        c("Rule 18", expertOrientR:::rule.18)
     )
     
+    # Rules not implemented: 15, 16, 17
+    unimplemented_rules <- c(15, 16, 17)
+    for (r in unimplemented_rules) {
+        if (rules[r]) {
+            cat("Warning: Rule", r, "is not implemented and will be skipped.\n")
+            rules[r] <- FALSE
+        }
+    }
     
     neighbors <- expertOrientR:::find_neighbors(pag)
     
@@ -80,4 +94,9 @@ complete_pag <- function(pag, rules=NULL, verbose=FALSE, debug=FALSE) {
     
     output <- list(pag=pag, counter=counter)
     return(output)
+}
+
+
+rule.null <- function(pag, unfVect=NULL, verbose=FALSE) {
+    return(pag)
 }
